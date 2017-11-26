8e28fd261667ff9113c7f063288c36bdae3cc335
HEO\HEO <hki1991@gmail.com>
2017-11-26 20:34:10 +0900

    유닛 클래스 함수 헤더에서 본문으로 이동

--- Unit.cpp
+++ Unit.cpp
@@ -31,3 +31,54 @@ void Unit::init(void)
 
 	ZeroMemory(&_atkRange, sizeof(_atkRange));
 }
+
+void Unit::setImgFace(int num)
+{
+	_numImagFace = num;
+	TCHAR strFaceKey[100];
+	_stprintf(strFaceKey, L"face %05d", _numImagFace);
+	_imgFace = IMAGEMANAGER->findImage(strFaceKey);
+}
+void Unit::setImgNormal(int num) {  }
+void Unit::setImgBattleIdle(int num)
+{
+	_battleState.numImgBattleIdle = num;
+	TCHAR strKey[100];
+	if (num < UNIT_BATTLE_IMAGE1)
+	{
+		_stprintf(strKey, L"unit%d-atk", _battleState.numImgBattleIdle);
+	}
+	else
+	{
+		_stprintf(strKey, L"unit%d-%d-atk", _battleState.numImgBattleIdle, _team);
+	}
+	_battleState.imgBattleAtk = IMAGEMANAGER->findImage(strKey);
+}
+void Unit::setImgBattleAtk(int num)
+{
+	_battleState.numImgBattleAtk = num;
+	TCHAR strKey[100];
+	if (num < UNIT_BATTLE_IMAGE1)
+	{
+		_stprintf(strKey, L"unit%d-atk", _battleState.numImgBattleAtk);
+	}
+	else
+	{
+		_stprintf(strKey, L"unit%d-%d-atk", _battleState.numImgBattleAtk, _team);
+	}
+	_battleState.imgBattleAtk = IMAGEMANAGER->findImage(strKey);
+}
+void Unit::setImgBattleSpc(int num)
+{
+	_battleState.numImgBattleSpc = num;
+	TCHAR strKey[100];
+	if (num < UNIT_BATTLE_IMAGE1)
+	{
+		_stprintf(strKey, L"unit%d-atk", _battleState.numImgBattleSpc);
+	}
+	else
+	{
+		_stprintf(strKey, L"unit%d-%d-atk", _battleState.numImgBattleSpc, _team);
+	}
+	_battleState.imgBattleAtk = IMAGEMANAGER->findImage(strKey);
+}
